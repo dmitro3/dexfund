507fc1d6fc6ffe4e9852c684f0649dedf2b3462d
domambia <omambiadauglous@gmail.com>
2021-10-19 16:39:05 +0300

    Adding Web3 Configuratiions

---
+++ src/ethereum/web3.js
@@ -0,0 +1,6 @@
+
+ // const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+// // Prompt user for account connections
+// await provider.send("eth_requestAccounts", []);
+// const signer = provider.getSigner();
+// console.log("Account:", await signer.getAddress());
