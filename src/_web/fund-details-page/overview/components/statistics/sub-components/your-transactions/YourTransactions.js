6f69a7d24f4dc263181f3e3b25d94bfa2d8ac2af
Omambia Dauglous <omambiadauglous@gmail.com>
2021-10-30 19:20:42 +0300

    Team fixes

--- src/_web/fund-details-page/overview/components/statistics/sub-components/your-transactions/YourTransactions.js
+++ src/_web/fund-details-page/overview/components/statistics/sub-components/your-transactions/YourTransactions.js
@@ -1,86 +1,88 @@
-import React, { Component } from 'react';
-import { getEthPrice, getTransactions } from '../../../../../../../ethereum/funds/fund-related';
-import {getTimeDiff} from '../../../../../../../ethereum/utils'
+import React, { Component } from "react";
+import {
+  getEthPrice,
+  getTransactions,
+} from "../../../../../../../ethereum/funds/fund-related";
+import { getTimeDiff } from "../../../../../../../ethereum/utils";
 
 // COMPONENTS
-import YourTransactionsTableHeader from './sub-components/YourTransactionsTableHeader';
-import YourTransactionsTableRow from './sub-components/YourTransactionsTableRow';
+import YourTransactionsTableHeader from "./sub-components/YourTransactionsTableHeader";
+import YourTransactionsTableRow from "./sub-components/YourTransactionsTableRow";
 
 // ASSETS
 // ...
 
 // CSS
-import '../your-transactions/styles/yourTransactions.css';
+import "../your-transactions/styles/yourTransactions.css";
 
 class YourTransactions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactionHistory: [],
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            transactionHistory: []
-        }
+  componentDidUpdate(prevProps) {
+    if (prevProps.account !== this.props.account) {
+      this.getData();
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if(prevProps.account !== this.props.account) {
-            this.getData();
-        }
+  callbackFunction = (childData) => {
+    this.setState({ searchedValue: childData });
+  };
+
+  isConnected() {
+    return this.props.onboard.walletConnected && this.props.onboard.provider;
+  }
+
+  async getData() {
+    await this.setState({ isLoaded: false });
+    if (this.isConnected()) {
+      let _ethPrice = await getEthPrice();
+      let trs = await getTransactions(this.props.onboard.address);
+      // let trs = [];
+      this.setState({
+        transactionHistory: trs || [],
+        ethPrice: _ethPrice,
+        isLoaded: true,
+      });
+    } else {
+      this.setState({
+        transactionHistory: [],
+        isLoaded: true,
+      });
     }
+  }
 
-    callbackFunction = (childData) => {
-        this.setState({ searchedValue: childData })
-    }
+  async componentDidMount() {
+    await this.getData();
+  }
 
-    isConnected() {
-        return this.props.account.account && this.props.account.connectSuccess;
-    }
-
-    async getData() {
-        await this.setState({ isLoaded: false })
-        if (this.isConnected()) {
-            let _ethPrice = await getEthPrice();
-            let trs = await getTransactions(this.props.account.account.address);
-            // let trs = [];
-            this.setState({
-                transactionHistory: trs || [],
-                ethPrice: _ethPrice,
-                isLoaded: true
-            });
-        } else {
-            this.setState({
-                transactionHistory: [],
-                isLoaded: true
-            })
-        }
-    }
-
-    async componentDidMount() {
-        await this.getData();
-    }
-
-    render() {
-
-        return(
-            
-            <>
-                <div className="w-fund-statistics-your-transactions-wrapper">
-                    <YourTransactionsTableHeader />
-                    {
-                        this.state.transactionHistory.map(transaction => (
-                            <YourTransactionsTableRow
-                                actionFromParent={transaction.type}
-                                tokenFromParent={parseFloat(transaction.value).toFixed(2)}
-                                valueFromParent={(parseFloat(transaction.value) * this.state.ethPrice).toFixed(2)}
-                                vaultFromParent={transaction.fundName}
-                                typeFromParent={transaction.type}
-                                timeFromParent={getTimeDiff(parseInt(transaction.timestamp) * 1000)}
-                            />
-                        ))
-                        }
-                </div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="w-fund-statistics-your-transactions-wrapper">
+          <YourTransactionsTableHeader />
+          {this.state.transactionHistory.map((transaction) => (
+            <YourTransactionsTableRow
+              actionFromParent={transaction.type}
+              tokenFromParent={parseFloat(transaction.value).toFixed(2)}
+              valueFromParent={(
+                parseFloat(transaction.value) * this.state.ethPrice
+              ).toFixed(2)}
+              vaultFromParent={transaction.fundName}
+              typeFromParent={transaction.type}
+              timeFromParent={getTimeDiff(
+                parseInt(transaction.timestamp) * 1000
+              )}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
 export default YourTransactions;
--- src/_web/home-page/top-investment-funds/TopInvestmentFunds.js
+++ src/_web/home-page/top-investment-funds/TopInvestmentFunds.js
@@ -1,76 +1,82 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-
-import { activateLoaderOverlay, deactivateLoaderOverlay } from '../../../redux/actions/LoaderAction';
-import configs from '../../../config';
-import { getFiveInvestments } from '../../../sub-graph-integrations';
+import {
+  activateLoaderOverlay,
+  deactivateLoaderOverlay,
+} from "../../../redux/actions/LoaderAction";
+import configs from "../../../config";
+import { getFiveInvestments } from "../../../sub-graph-integrations";
 
 // COMPONENTS
-import MostProfitableAllTime from './components/MostProfitableAllTime';
-import MostProfitableThisMonth from './components/MostProfitableThisMonth';
-import MostProfitableToday from './components/MostProfitableToday';
+import MostProfitableAllTime from "./components/MostProfitableAllTime";
+import MostProfitableThisMonth from "./components/MostProfitableThisMonth";
+import MostProfitableToday from "./components/MostProfitableToday";
 
 // ASSETS
 // ...
 
 // CSS
-import './styles/topInvestmentFunds.css';
+import "./styles/topInvestmentFunds.css";
 
 class TopInvestmentFunds extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      investments: [],
+    };
+  }
 
-        this.state = {
-            investments: []
-        }
-    }
+  async componentDidMount() {
+    var investments = await getFiveInvestments();
+    this.setState({
+      ...this.state,
+      investments: investments,
+    });
+  }
 
-    async componentDidMount() {
-        var investments =  await getFiveInvestments();
-        console.log("123123",investments)
-        this.setState({
-            ...this.state,
-            investments: investments
-        })
-    }
-
-
-    render() {
-
-        return (
-
-            <>
-                <div className="w-top-investment-funds-wrapper">
-                    <div className="w-top-investment-funds-header">
-                        TOP INVESTMENT FUNDS
-                    </div>
-                   {
-                       this.props.onboard.walletConnected ? ( <div className="w-top-investment-funds-content">
-                       <MostProfitableAllTime investments={this.state.investments} {...this.props} />
-                       <MostProfitableThisMonth investments={this.state.investments} {...this.props} />
-                       <MostProfitableToday investments={this.state.investments} {...this.props} />
-                   </div>) : <div> </div>
-                   }
-                </div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="w-top-investment-funds-wrapper">
+          <div className="w-top-investment-funds-header">
+            TOP INVESTMENT FUNDS
+          </div>
+          {this.props.onboard.walletConnected ? (
+            <div className="w-top-investment-funds-content">
+              <MostProfitableAllTime
+                investments={this.state.investments}
+                {...this.props}
+              />
+              <MostProfitableThisMonth
+                investments={this.state.investments}
+                {...this.props}
+              />
+              <MostProfitableToday
+                investments={this.state.investments}
+                {...this.props}
+              />
+            </div>
+          ) : (
+            <div> </div>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        account: state.connect,
-    };
+  return {
+    account: state.connect,
+    onboard: state.onboard,
+  };
 };
-
 
 const mapDispatchToProps = {
-    activateLoaderOverlay, 
-    deactivateLoaderOverlay
+  activateLoaderOverlay,
+  deactivateLoaderOverlay,
 };
 
-
 export default connect(mapStateToProps, mapDispatchToProps)(TopInvestmentFunds);
-
