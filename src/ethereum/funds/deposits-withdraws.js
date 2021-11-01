da3c02d24861f9d44a5626897a86d631d4c063ee
tudorog <tudor.posta@live.com>
2021-11-01 23:23:22 +0200

    allowance

--- src/ethereum/funds/deposits-withdraws.js
+++ src/ethereum/funds/deposits-withdraws.js
@@ -3,6 +3,54 @@ import FundActionsWrapper from './../abis/FundActionsWrapper.json';
 import VaultLib from './../abis/VaultLib.json';
 import ComptrollerLib from './../abis/ComptrollerLib.json';
 
+export const getDenominationAllowance = async (
+    fundAddress,
+    investor,
+    provider
+) => {
+    const VaultLibInterface = new ethers.utils.Interface(
+        JSON.parse(JSON.stringify(VaultLib.abi))
+    );
+    const ComptrollerLibInterface = new ethers.utils.Interface(
+        JSON.parse(JSON.stringify(ComptrollerLib.abi))
+    );
+
+    const vaultLibContract = new ethers.Contract(
+        fundAddress,
+        VaultLibInterface,
+        provider
+    );
+
+    // Estimation
+    const comptroller = await vaultLibContract.getAccessor();
+    const comptrollerContract = new ethers.Contract(
+        comptroller,
+        ComptrollerLibInterface,
+        provider
+    );
+    const denominationAsset = await comptrollerContract.getDenominationAsset();
+
+    // Use VaultLib interface for shares functions
+    const assetContract = new ethers.Contract(
+        denominationAsset,
+        VaultLibInterface,
+        provider
+    );
+
+    const allowance = await assetContract.allowance(investor, comptroller);
+
+    return allowance;
+}
+
+export const investFundDenomination = async (
+    fundAddress,
+    investor,
+    provider,
+    amount
+) => {
+
+}
+
 // export const investFundEth = async (
 //     fundAddress,
 //     amount,
